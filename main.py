1649402822bce407d05d770a59f1326b01001c7f
장지현 <134585955+treejh@users.noreply.github.com>
2024-12-10 17:53:07 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import feedparser, datetime
+
+uri="https://eun2ce.tistory.com"
+feed = feedparser.parse(uri+"/rss")
+
+markdown_text = """# eun2ce.tistory.com
+## blog posts
+"""
+
+lst = []
+
+
+for i in feed['entries']:
+    dt = datetime.datetime.strptime(i['published'], "%a, %d %b %Y %H:%M:%S %z").strftime("%b %d, %Y")
+    markdown_text += f"[{i['title']}]({i['link']}) - {dt}<br>\n"
+    print(i['link'], i['title'])
+
+f = open("README.md",mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
